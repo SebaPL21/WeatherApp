2e0910b0818aedb3613a1c2fc2ebb871573abafb
SebaPL21 <sebastian19993152@gmail.com>
2022-04-30 17:56:06 +0200

    sipmle login form

--- weather_app/src/views/HomeView.vue
+++ weather_app/src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<b-container>
+ <b-row class="bg-page">
+    <b-col cols="8">
+      <h1>Miasta</h1>
+      <h4>Sprawdź pogodę dla twojego miasta</h4>
+
+
+    </b-col>
+    <b-col cols="4"><h2>Użytkownika</h2> </b-col>
+  </b-row>
+</b-container>
+ 
+
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
 @Options({
   components: {
-    HelloWorld,
+   
   },
 })
 export default class HomeView extends Vue {}
 </script>
+<style scoped>
+
+.bg-page{
+  margin-top: 40px;
+  border-radius: 30px;
+  background-color: aliceblue;
+}
+</style>

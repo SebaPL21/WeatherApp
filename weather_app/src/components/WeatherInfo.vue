2d373ac9008469eb7b6d8b5d3340d464c8265d3b
SebaPL21 <sebastian19993152@gmail.com>
2022-05-02 22:40:52 +0200

    template for cities

---
+++ weather_app/src/components/WeatherInfo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+     <div class="date-hours"> 12,sep 
+        <h3>12:23</h3> 
+     </div>
+    </div>
+
+      <h3>nazwa miejscowości</h3>
+
+    <div class="act_temp">temperatuta aktualna
+      <div class="fell_temp">temperatuta odczuwalna </div> 
+    </div>
+
+    <div class="desc">
+      <p> ćiśnienie</p>   
+      <p> wilgotność</p>   
+      <p> wiatr</p>
+    </div>
+</template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+export default class WeatherInfo extends Vue { }
+</script>
